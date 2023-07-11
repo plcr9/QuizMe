9ad4b7d5fcecc3f560c3a9ee8bb2d696b1628c0d
Paul72187 <paulcooper@Pauls-MacBook-Air.local>
2023-07-11 15:07:37 +0100

    game.js amendments

--- game.js
+++ game.js
@@ -2,12 +2,12 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
 let questions = [{
         question: "Who was the first Estonian footballer to score an English Premier League goal?",
@@ -38,50 +38,51 @@ let questions = [{
 
 //Constants
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
+    availableQuesions = [...questions];
     getNewQuestion();
-}
+};
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        return window.location.assign("end.html")
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
+        //go to the end page
+        return window.location.assign("end.html");
     }
     questionCounter++;
-    progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
+    choices.forEach((choice) => {
         const number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
     });
 
-    availableQuestions.splice(questionIndex, 1);
-
+    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
-
 };
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
+choices.forEach((choice) => {
+    choice.addEventListener("click", (e) => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-        if (classToApply === 'correct') {
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        if (classToApply === "correct") {
             incrementScore(CORRECT_BONUS);
         }
 
@@ -94,9 +95,7 @@ choices.forEach(choice => {
     });
 });
 
-incrementScore = num => {
+incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
-}
-
-startGame();
+};
